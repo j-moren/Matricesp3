fab108ba244f19bc6aca788be5e58035af810e6e
J-Moren <kalev.1503@gmail.com>
2025-02-17 22:39:34 -0500

    Método Solicitar Dimensión

---
+++ matrizSuma.java
@@ -0,0 +1,11 @@
+// Realice un algoritmo que llene una matriz de 5 * 5. Calcular la suma de cada fila y almacenarla en un vector, la suma de cada columna y almacenarla en otro vector
+
+public class matrizSuma {
+    
+    public static void main (String[] args){
+
+        
+
+    }
+    
+}
